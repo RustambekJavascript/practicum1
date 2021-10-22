d7a654913424add3d9466bd19e2b378f72cf9151
RustambekJavascript <rustambekeshboriyev@gmail.com>
2021-10-22 20:58:18 +0500

    practicum site first commits

---
+++ menu/js/main.js
@@ -0,0 +1,34 @@
+function dropdownStart1() {
+  var c = document.getElementById("dropdown-menu1").style.display;
+  if (c == "block") {
+    document.getElementById("dropdown-menu1").style.display = "none";
+  } else {
+    document.getElementById("dropdown-menu1").style.display = "block";
+  }
+}
+
+function dropdownStart() {
+  var b = document.getElementById("dropdown-menu").style.display;
+  if (b == "block") {
+    document.getElementById("dropdown-menu").style.display = "none";
+  } else {
+    document.getElementById("dropdown-menu").style.display = "block";
+  }
+}
+
+function menu() {
+  var a = document.getElementById("ul-menu").style.display;
+  if (a == "none") {
+    document.getElementById("ul-menu").style.display = "flex";
+  } else {
+    document.getElementById("ul-menu").style.display = "none";
+  }
+}
+
+// function order() {
+//   var order = (document.getElementById("order").style.display = "block");
+//   var btn = (document.getElementById("btn").style.display = "none");
+//   if(order == "none" ){
+
+//   }
+// }
