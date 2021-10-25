b20d11d959984e5820b548ccc0ea5f7f208c1e24
RustambekJavascript <rustambekeshboriyev@gmail.com>
2021-10-25 07:30:05 +0500

    yangilandi

--- contact/js/contactn.js
+++ contact/js/contactn.js
@@ -1,27 +1,48 @@
 function dropdownStart1() {
-    var c = document.getElementById("dropdown-menu1").style.display;
-    if (c == "block") {
-      document.getElementById("dropdown-menu1").style.display = "none";
-    } else {
-      document.getElementById("dropdown-menu1").style.display = "block";
-    }
+  var c = document.getElementById("dropdown-menu1").style.display;
+  if (c == "block") {
+    document.getElementById("dropdown-menu1").style.display = "none";
+  } else {
+    document.getElementById("dropdown-menu1").style.display = "block";
   }
-  
-  function dropdownStart() {
-    var b = document.getElementById("dropdown-menu").style.display;
-    if (b == "block") {
-      document.getElementById("dropdown-menu").style.display = "none";
-    } else {
-      document.getElementById("dropdown-menu").style.display = "block";
-    }
+}
+
+function dropdownStart() {
+  var b = document.getElementById("dropdown-menu").style.display;
+  if (b == "block") {
+    document.getElementById("dropdown-menu").style.display = "none";
+  } else {
+    document.getElementById("dropdown-menu").style.display = "block";
   }
-  
-  function menu() {
-    var a = document.getElementById("ul-menu").style.display;
-    if (a == "none") {
-      document.getElementById("ul-menu").style.display = "flex";
-    } else {
-      document.getElementById("ul-menu").style.display = "none";
-    }
+}
+
+function menu() {
+  var a = document.getElementById("ul-menu").style.display;
+  if (a == "none") {
+    document.getElementById("ul-menu").style.display = "flex";
+  } else {
+    document.getElementById("ul-menu").style.display = "none";
   }
-  
+}
+var x = document.getElementById("screen-menu");
+var y = document.getElementById("body1");
+var m = document.getElementById("modal");
+var b = document.getElementById("backet");
+
+function button(params) {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function chaqir(params) {
+  m.style.display = "flex";
+}
+function yop(params) {
+  m.style.display = "none";
+  b.style.display = "none";
+}
+function backetOpen(params) {
+  b.style.display = "flex";
+}
--- search/search.js
+++ search/search.js
@@ -24,3 +24,26 @@ function menu() {
     document.getElementById("ul-menu").style.display = "none";
   }
 }
+
+var x = document.getElementById("screen-menu");
+var y = document.getElementById("body1");
+var m = document.getElementById("modal");
+var b = document.getElementById("backet");
+
+function button(params) {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function chaqir(params) {
+  m.style.display = "flex";
+}
+function yop(params) {
+  m.style.display = "none";
+  b.style.display = "none";
+}
+function backetOpen(params) {
+  b.style.display = "flex";
+}
